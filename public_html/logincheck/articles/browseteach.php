8ffe8f1cfb16f031c9430f6fd149cad11c37d0de
soumyajyoti <soumyajyoti.naskar@innoraft.com>
2014-10-30 14:34:07 +0530

    completed login registration landing page

---
+++ public_html/logincheck/articles/browseteach.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>News-article</title>
+<link rel="stylesheet" type="text/css" href="browseteach.css" />
+</head>
+<body>
+<div id="banner">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="logo.jpg" width="200px" height="45px"/>
+</div>
+<div id="main">
+<div id="top">
+checking top
+</div>
+<div id="left">
+checking left
+</div>
+<div id="secondleft">
+checking secondleft
+</div>
+<div id="right">
+checking right
+</div>
+<div id="down">
+checking down
+</div>
+</div>
+
+
+
+</body>
+</html>
